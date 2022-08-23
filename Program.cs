663834d0b9661cc18cbd97ecd8fb1775bf656edf
steklovaDM <108463196+steklovaDM@users.noreply.github.com>
2022-08-23 21:28:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*using System;
+ 
+class Program {
+ public static int Main() {
+  //число, которое нужно возвести в степень
+  Console.Write("Введите число   : ");
+  int num = int.Parse(Console.ReadLine());
+  //степень числа
+  Console.Write("Введите степень : ");
+  int n = int.Parse(Console.ReadLine());
+  //число, возведенное в степень
+  int num_n=1;
+  for(int i=0; i<n; i++) {
+   num_n*=num;
+  }
+  Console.WriteLine("{0} ^ {1} = {2}", num, n, num_n);
+  Console.ReadKey();
+  return 0;
+ }
+}
+*/
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+     s = s + a % 10;
+    a = a /10 ;
+ }
+ Console.WriteLine(s);
+ */
+ 
